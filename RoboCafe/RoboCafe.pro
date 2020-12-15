97305bb5e8b837789b913993f0aa25179058859f
XidaZ3 <59092936+XidaZ3@users.noreply.github.com>
2020-12-15 17:16:37 +0100

    Revert "Risorse.h"
    This reverts commit 57d80969f0d0e4fc88d5f78519623eb66b75b0a1.

--- RoboCafe/RoboCafe.pro
+++ RoboCafe/RoboCafe.pro
@@ -9,15 +9,13 @@ CONFIG += c++17
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
-    Risorse.cpp \
     main.cpp \
     mainwindow.cpp
 
 HEADERS += \
     DeepPtr.h \
-    Risorse.h \
-    Vettore.h \
-    mainwindow.h
+    mainwindow.h \
+    vettore.h
 
 FORMS += \
     mainwindow.ui
--- RoboCafe/vettore.h
+++ RoboCafe/vettore.h
@@ -4,18 +4,18 @@
 #include <iostream>
 #include <math.h>
 
-template <class T> class Vettore;
+template <class T> class vettore;
 
 template <class T>
-std::ostream& operator<< (std::ostream& os, const Vettore<T>& val);
+std::ostream& operator<< (std::ostream& os, const vettore<T>& val);
 
 template <class T>
-class Vettore
+class vettore
 {
-    friend std::ostream& operator<< <T>(std::ostream& os, const Vettore<T>& val);
+    friend std::ostream& operator<< <T>(std::ostream& os, const vettore<T>& val);
     private:
     class iterator{
-        friend class Vettore<T>;
+        friend class vettore<T>;
         public:
             T* ptr;
             iterator();
@@ -39,15 +39,15 @@ class Vettore
     static int spaceNeeded(const unsigned int k);
 
     public:
-    Vettore<T>(unsigned int k=0, const T& val= T());
-    ~Vettore<T>();
-    Vettore<T>(const Vettore<T>& other);
-    Vettore<T>& operator=(const Vettore<T>& other);
-    Vettore<T> operator+(const Vettore<T>& other) const;
-    bool operator==(const Vettore<T>& other) const;
-    bool operator<(const Vettore<T>& other)const ;
+    vettore<T>(unsigned int k=0, const T& val= T());
+    ~vettore<T>();
+    vettore<T>(const vettore<T>& other);
+    vettore<T>& operator=(const vettore<T>& other);
+    vettore<T> operator+(const vettore<T>& other) const;
+    bool operator==(const vettore<T>& other) const;
+    bool operator<(const vettore<T>& other)const ;
     T& operator[](unsigned int k)const;
-    Vettore<T>& append(const Vettore<T>& other);
+    vettore<T>& append(const vettore<T>& other);
     void resize(unsigned int value);
     unsigned int getSize() const;
     unsigned int getCapacity() const;
@@ -61,17 +61,17 @@ class Vettore
 };
 
 template <class T>
-unsigned int Vettore<T>::getCapacity() const{
+unsigned int vettore<T>::getCapacity() const{
     return available;
 }
 
 template <class T>
-int Vettore<T>::spaceNeeded(const unsigned int k){
+int vettore<T>::spaceNeeded(const unsigned int k){
     return pow(2,ceil(log2(k)));
 }
 
 template <class T>
-Vettore<T>::Vettore(unsigned int k, const T& init) : available(spaceNeeded(k)),arr(k ? new T[available] : nullptr), occupied(k) {
+vettore<T>::vettore(unsigned int k, const T& init) : available(spaceNeeded(k)),arr(k ? new T[available] : nullptr), occupied(k) {
     first = arr;
     for(unsigned int i=0;i<occupied; i++)
     {
@@ -81,13 +81,13 @@ Vettore<T>::Vettore(unsigned int k, const T& init) : available(spaceNeeded(k)),a
 }
 
 template <class T>
-Vettore<T>::~Vettore() {
+vettore<T>::~vettore() {
     if(arr) delete[] arr;
     first = last = nullptr;
 }
 
 template <class T>
-Vettore<T>::Vettore(const Vettore& other) : available(other.available) , occupied(other.occupied){
+vettore<T>::vettore(const vettore& other) : available(other.available) , occupied(other.occupied){
     delete[] arr;
     arr = other.occupied? new T[other.available]:nullptr;
     first = arr;
@@ -99,7 +99,7 @@ Vettore<T>::Vettore(const Vettore& other) : available(other.available) , occupie
 }
 
 template <class T>
-Vettore<T>& Vettore<T>::operator=(const Vettore& other) {
+vettore<T>& vettore<T>::operator=(const vettore& other) {
     if(this  != &other)
     {
         delete [] arr;
@@ -117,14 +117,14 @@ Vettore<T>& Vettore<T>::operator=(const Vettore& other) {
 }
 
 template <class T>
-Vettore<T> Vettore<T>::operator+(const Vettore& other) const {
-    Vettore<T> aux(*this);
+vettore<T> vettore<T>::operator+(const vettore& other) const {
+    vettore<T> aux(*this);
     aux.append(other);
     return aux;
 }
 
 template <class T>
-bool Vettore<T>::operator==(const Vettore<T>& other)const {
+bool vettore<T>::operator==(const vettore<T>& other)const {
     if(this == &other) return true;
     if(occupied != other.occupied || available != other.available) return false;
     for(int i=0; i<occupied; i++){
@@ -133,21 +133,21 @@ bool Vettore<T>::operator==(const Vettore<T>& other)const {
 }
 
 template <class T>
-T& Vettore<T>::operator[](unsigned int k)const {
+T& vettore<T>::operator[](unsigned int k)const {
     return *(arr+k);
 }
 
 template <class T>
-void Vettore<T>::resize(unsigned int value){
+void vettore<T>::resize(unsigned int value){
     if(value){
-        Vettore<T>* aux = new Vettore<T>(*this);
+        vettore<T>* aux = new vettore<T>(*this);
         aux->available = spaceNeeded(value);
         *this = *aux;
     }
 }
 
 template <class T>
-Vettore<T>& Vettore<T>::append(const Vettore<T>& other){
+vettore<T>& vettore<T>::append(const vettore<T>& other){
     if(other.occupied != 0){
         T* ret = new T[available = spaceNeeded(occupied+other.occupied)];
         for(unsigned int i=0; i<occupied; ++i){
@@ -166,7 +166,7 @@ Vettore<T>& Vettore<T>::append(const Vettore<T>& other){
 }
 
 template <class T>
-T* Vettore<T>::copia(const T* other, unsigned int occupied, unsigned int available){
+T* vettore<T>::copia(const T* other, unsigned int occupied, unsigned int available){
     if(other !=nullptr)
     {
         T* ret = new T[available];
@@ -177,15 +177,15 @@ T* Vettore<T>::copia(const T* other, unsigned int occupied, unsigned int availab
 }
 
 template <class T>
-void Vettore<T>::distruggi(const T* other){
+void vettore<T>::distruggi(const T* other){
     delete[] other;
 }
 
 template <class T>
-unsigned int Vettore<T>::getSize()const {return occupied;}
+unsigned int vettore<T>::getSize()const {return occupied;}
 
 template <class T>
-std::ostream& operator<< (std::ostream& os, const Vettore<T>& val){
+std::ostream& operator<< (std::ostream& os, const vettore<T>& val){
     if(val.getSize() == 0) return os<< "[]";
     else{
         os<< "[";
@@ -198,13 +198,13 @@ std::ostream& operator<< (std::ostream& os, const Vettore<T>& val){
 }
 
 template <class T>
-Vettore<T>::iterator::iterator():ptr(nullptr){}
+vettore<T>::iterator::iterator():ptr(nullptr){}
 template <class T>
-Vettore<T>::iterator::iterator(T* p):ptr(p){}
+vettore<T>::iterator::iterator(T* p):ptr(p){}
 template <class T>
-Vettore<T>::iterator::iterator(const Vettore<T>::iterator& other):ptr(other.ptr){}
+vettore<T>::iterator::iterator(const vettore<T>::iterator& other):ptr(other.ptr){}
 template <class T>
-typename Vettore<T>::iterator& Vettore<T>::iterator::operator++ (){
+typename vettore<T>::iterator& vettore<T>::iterator::operator++ (){
     if(ptr!= nullptr) {
         ptr++;
         return *this;
@@ -212,47 +212,47 @@ typename Vettore<T>::iterator& Vettore<T>::iterator::operator++ (){
 
 }
 template <class T>
-typename Vettore<T>::iterator Vettore<T>::iterator::operator++ (int){
+typename vettore<T>::iterator vettore<T>::iterator::operator++ (int){
     if(ptr!= nullptr){
-        auto ret = Vettore<T>::iterator(*this);
+        auto ret = vettore<T>::iterator(*this);
         ptr++;
         return ret;
     }
 
 }
 template <class T>
-bool Vettore<T>::iterator::operator==(const Vettore<T>::iterator& other)const{
+bool vettore<T>::iterator::operator==(const vettore<T>::iterator& other)const{
     return (ptr == other.ptr);
 }
 template <class T>
-bool Vettore<T>::iterator::operator!=(const Vettore<T>::iterator& other)const{
+bool vettore<T>::iterator::operator!=(const vettore<T>::iterator& other)const{
     return (ptr != other.ptr);
 }
 template <class T>
-typename Vettore<T>::iterator& Vettore<T>::iterator::operator= (const Vettore<T>::iterator& other){
+typename vettore<T>::iterator& vettore<T>::iterator::operator= (const vettore<T>::iterator& other){
     if(this != &other)
     {
         ptr = other.ptr;
     }return *this;
 }
 template <class T>
-T& Vettore<T>::iterator::operator* ()const{
+T& vettore<T>::iterator::operator* ()const{
     return *ptr;
 }
 template <class T>
-T* Vettore<T>::iterator::operator-> ()const{
+T* vettore<T>::iterator::operator-> ()const{
     return &(*ptr);
 }
 
 template <class T>
-void Vettore<T>::push_back(const T& value){
+void vettore<T>::push_back(const T& value){
     if(occupied <available){
         arr[occupied]=value;
         occupied++;
     }
 }
 template <class T>
-T& Vettore<T>::pop(){
+T& vettore<T>::pop(){
     T* ret = new T();
     if(occupied){
         occupied--;
@@ -261,7 +261,7 @@ T& Vettore<T>::pop(){
     return *ret;
 }
 template <class T>
-T& Vettore<T>::erase(unsigned int index){
+T& vettore<T>::erase(unsigned int index){
     T* ret = new T(arr[index]);
     for(unsigned int i= index; i<occupied; i++){
         arr[i]=arr[i+1];
@@ -270,7 +270,7 @@ T& Vettore<T>::erase(unsigned int index){
     return *ret;
 }
 template <class T>
-void Vettore<T>::insert(T& value, unsigned int index){
+void vettore<T>::insert(T& value, unsigned int index){
     if(occupied<available)
         arr[occupied+1]=arr[occupied];
     for(unsigned int i = occupied; i>index; i--)
@@ -278,18 +278,18 @@ void Vettore<T>::insert(T& value, unsigned int index){
     arr[index] = value;
 }
 template <class T>
-unsigned int Vettore<T>::find(const T& value)const{
+unsigned int vettore<T>::find(const T& value)const{
     for(unsigned int i=0; i<occupied; i++)
         if(arr[i]== value) return i;
     return -1;
 }
 template <class T>
-typename Vettore<T>::iterator Vettore<T>::begin() const{
+typename vettore<T>::iterator vettore<T>::begin() const{
     std::cout<<"Begin: "<<&first<<"  "<<first<<std::endl;
     return iterator(first);
 }
 template <class T>
-typename Vettore<T>::iterator Vettore<T>::end() const {
+typename vettore<T>::iterator vettore<T>::end() const {
     if(!last) return iterator();
     return iterator(last+1);
 }
