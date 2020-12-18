a6c622954bf427f43e47b4c4fbdff140a04b5565
XidaZ3 <59092936+XidaZ3@users.noreply.github.com>
2020-12-18 20:44:12 +0100

    Fix warning

--- RoboCafe/RoboCafe.pro
+++ RoboCafe/RoboCafe.pro
@@ -2,7 +2,7 @@ QT       += core gui
 
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 
-CONFIG += c++17
+CONFIG += c++11
 
 # You can make your code fail to compile if it uses deprecated APIs.
 # In order to do so, uncomment the following line.
@@ -41,10 +41,9 @@ HEADERS += \
     Vettore.h \
     mainwindow.h
 
-FORMS += \
-    mainwindow.ui
-
 # Default rules for deployment.
 qnx: target.path = /tmp/$${TARGET}/bin
 else: unix:!android: target.path = /opt/$${TARGET}/bin
 !isEmpty(target.path): INSTALLS += target
+
+DISTFILES +=
--- RoboCafe/main.cpp
+++ RoboCafe/main.cpp
@@ -15,29 +15,29 @@ const float Dipendente::sconto = 0.8f;
 
 int main(int argc, char *argv[])
 {
-//    Vettore<int> v1;
-//    Vettore<int> v2(5,6);
-//    v1.append(v2).append(Vettore<int>(2,3));
-//    std::cout<<v1<<std::endl<<v2<<std::endl;
+    Vettore<int> v1;
+    Vettore<int> v2(5,6);
+    v1.append(v2).append(Vettore<int>(2,3));
+    std::cout<<v1<<std::endl<<v2<<std::endl;
 
-//    int conto=100;
-//    ClientePlus palazzinaro;
-//    ClienteStandard povero;
-//    Dipendente dip;
+    int conto=100;
+    ClientePlus palazzinaro;
+    ClienteStandard povero;
+    Dipendente dip;
 
-//    palazzinaro.Ricarica(100);
-//    povero.Ricarica(100);
-//    dip.Ricarica(100);
+    palazzinaro.Ricarica(100);
+    povero.Ricarica(100);
+    dip.Ricarica(100);
 
-//    palazzinaro.Pagamento(conto);
-//    povero.Pagamento(conto);
-//    dip.Pagamento(conto);
+    palazzinaro.Pagamento(conto);
+    povero.Pagamento(conto);
+    dip.Pagamento(conto);
 
-//    cout<<"palazzinaro:"<<palazzinaro.getCredito()
-//       <<" povero:"<<povero.getCredito()
-//      <<" dip:"<<dip.getCredito()<<endl;
+    cout<<"palazzinaro:"<<palazzinaro.getCredito()
+       <<" povero:"<<povero.getCredito()
+      <<" dip:"<<dip.getCredito()<<endl;
 
-//    cout<<"punti"<<palazzinaro.getPunti();
+    cout<<"punti"<<palazzinaro.getPunti()<<endl;
 
     QApplication a(argc, argv);
     MainWindow w;
